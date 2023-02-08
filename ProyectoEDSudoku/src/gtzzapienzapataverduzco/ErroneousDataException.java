1ba5e0446df3cf893f72a426eb25256905485d79
MarielSoGuZa <marielsgtzz@gmail.com>
2023-02-08 10:29:12 -0600

    Eliminacion sub-folders

---
+++ ProyectoEDSudoku/src/gtzzapienzapataverduzco/ErroneousDataException.java
@@ -0,0 +1,13 @@
+package gtzzapienzapataverduzco;
+
+/**
+ * @authors Mariel S. Gtz. Zapien, Mariana Zapata Covarrubias & Mauricio Verduzco Chavira
+ */
+
+public class ErroneousDataException extends RuntimeException {
+    
+    public ErroneousDataException (String s) {
+      super ("Los datos son erroneos: " + s);
+   }
+    
+}
